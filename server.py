5a53cfc7cfd685f99d9db02f8246b6fc8735cf16
tejudaya <tudayakumar@wsgc.com>
2025-08-19 14:32:55 -0700

    Update server.py

--- server.py
+++ server.py
@@ -29,8 +29,38 @@ class IntentRequest(BaseModel):
 class Product(BaseModel):
     id: Optional[str] = None
     title: Optional[str] = None
-    link: Optional[str] = None
-    image: Optional[str] = None
+    url: Optional[str] = None
+    image_url: Optional[str] = None
+    matched_terms: Optional[List[str]] = None
+    is_slotted: Optional[bool] = None
+    labels: Optional[dict] = None
+    variation_id: Optional[str] = None
+    description: Optional[str] = None
+    product_blurb: Optional[str] = None
+    product_info: Optional[str] = None
+    lowest_price: Optional[float] = None
+    highest_price: Optional[float] = None
+    sale_price_min: Optional[float] = None
+    sale_price_max: Optional[float] = None
+    regular_price_min: Optional[float] = None
+    regular_price_max: Optional[float] = None
+    product_price_type: Optional[str] = None
+    swatch_prices: Optional[dict] = None
+    min_avl_now_pl: Optional[str] = None
+    leader_sku: Optional[str] = None
+    leader_sku_image: Optional[str] = None
+    thumb_image: Optional[str] = None
+    thumb_image_parent: Optional[str] = None
+    image_override: Optional[str] = None
+    alt_images: Optional[List[str]] = None
+    hover_images: Optional[List[str]] = None
+    image_roll_overs: Optional[List[str]] = None
+    swatches_display: Optional[dict] = None
+    group_ids: Optional[List[str]] = None
+    facets: Optional[List[dict]] = None
+    flags: Optional[List[str]] = None
+    pip_type: Optional[str] = None
+    eligible_for_quick_buy: Optional[bool] = None
 
 class IntentResponse(BaseModel):
     first_five_products: List[Product]
